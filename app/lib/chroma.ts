4d076f56d1117cfd4240930c75a9af4182637232
JakubHavlas <jakub.havlas.022@pslib.cz>
2026-02-06 21:47:42 +0100

    sdf

--- app/lib/chroma.ts
+++ app/lib/chroma.ts
@@ -2,6 +2,9 @@ import { ChromaClient } from "chromadb";
 
 const chromaUrl = process.env.CHROMA_URL || "http://localhost:8000";
 
+
 export const chroma = new ChromaClient({
-  path: chromaUrl,
+  host: chromaUrl,
+  port: 8000,
+  
 });
